ff94979fdfd2cb3cd6b76306df4416f95dfda51f
ramil <ramil2911912@gmail.com>
2021-08-04 18:13:40 +0500

    Index redesign

---
+++ src/components/SkeletonPanel.svelte
@@ -0,0 +1,17 @@
+<div class="animate-pulse">
+    <div class="flex flex-col rounded-lg bg-[#203040] h-[22rem] w-48 text-white cursor-pointer">
+        <div class="h-[68%] rounded-t-lg"/>
+        <div class="flex mx-1 flex-grow w-full">
+            <div class="flex flex-col pl-2 pt-2 pr-2 pb-1">
+                <p class="text-lg"></p>
+                <p></p>
+                <div class="flex content-center flex-wrap flex-grow gap-1">
+                    {#each Array(5) as _, i}
+                        <div class="text-xs rounded-lg bg-red-900 px-1 animate-pulse w-8">
+                        </div>
+                    {/each}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
 	import type { Movie } from '../models/Movie';
 	import MoviePanel from '../components/MoviePanel.svelte';
 	import { serverIps } from '../models/ServerIps';
+	import SkeletonPanel from '../components/SkeletonPanel.svelte';
 
 	async function fetchPopularMovies(): Promise<Movie[]> {
 		let movies: Movie[] = [];
@@ -19,21 +20,113 @@
 	var popularMovies = fetchPopularMovies();
 </script>
 
-<div>
-	<h2 class="text-4xl font-sans text-white mb-4">Фильмы</h2>
-	<div class="flex gap-4">
-		{#await popularMovies}
-			<div class="flex rounded-lg bg-gray-700 h-80 w-48 text-white">
-				<p class="text-lg">Waiting...</p>
+<div class="grid 2xl:grid-cols-2 gap-y-4 gap-x-1 text-white">
+	<div>
+		<p class="text-5xl mb-2">Фильмы</p>
+		<div class="flex justify-start">
+			<div class="h-[22rem] flex flex-wrap overflow-hidden gap-4 flex-initial">
+				{#await popularMovies}
+				<SkeletonPanel/>
+				{:then result}
+					{#each result as film}
+						<MoviePanel movie={film} />
+					{/each}
+				{:catch error}
+					<div class="flex rounded-lg bg-gray-700 h-80 w-48 text-white">
+						<p class="text-lg">{error.stack}</p>
+					</div>
+				{/await}
 			</div>
-		{:then result}
-			{#each result as film}
-				<MoviePanel movie={film} />
-			{/each}
-		{:catch error}
-			<div class="flex rounded-lg bg-gray-700 h-80 w-48 text-white">
-				<p class="text-lg">{error.stack}</p>
+			<div class="hover:cursor-pointer w-[40px] h-[36px] bg-[#283848] text-center rounded-full self-center flex items-center justify-center">
+				<span>></span>
 			</div>
-		{/await}
+		</div>
+	</div>
+	<div>
+		<p class="text-5xl mb-2">Сериалы</p>
+		<div class="flex justify-start">
+			<div class="h-[22rem] flex flex-wrap overflow-hidden gap-4 flex-grow-0">
+				{#await popularMovies}
+				<SkeletonPanel/>
+				{:then result}
+					{#each result as film}
+						<MoviePanel movie={film} />
+					{/each}
+				{:catch error}
+					<div class="flex rounded-lg bg-gray-700 h-80 w-48 text-white">
+						<p class="text-lg">{error.stack}</p>
+					</div>
+				{/await}
+			</div>
+			<div class="hover:cursor-pointer w-[40px] h-[36px] bg-[#283848] text-center rounded-full self-center flex items-center justify-center">
+				<span>></span>
+			</div>
+		</div>
+	</div>
+	<div>
+		<p class="text-5xl mb-2">Книги</p>
+		<div class="flex justify-start">
+			<div class="h-[22rem] flex flex-wrap overflow-hidden gap-4 flex-grow-0">
+				{#await popularMovies}
+				<SkeletonPanel/>
+				{:then result}
+					{#each result as film}
+						<MoviePanel movie={film} />
+					{/each}
+				{:catch error}
+					<div class="flex rounded-lg bg-gray-700 h-80 w-48 text-white">
+						<p class="text-lg">{error.stack}</p>
+					</div>
+				{/await}
+			</div>
+			<div class="hover:cursor-pointer w-[40px] h-[36px] bg-[#283848] text-center rounded-full self-center flex items-center justify-center">
+				<span>></span>
+			</div>
+		</div>
+	</div>
+	<div>
+		<p class="text-5xl mb-2">Персонажи</p>
+		<div class="flex justify-start">
+			<div class="h-[22rem] flex flex-wrap overflow-hidden gap-4 flex-grow-0">
+				{#await popularMovies}
+				<SkeletonPanel/>
+				{:then result}
+					{#each result as film}
+						<MoviePanel movie={film} />
+					{/each}
+				{:catch error}
+					<div class="flex rounded-lg bg-gray-700 h-80 w-48 text-white">
+						<p class="text-lg">{error.stack}</p>
+					</div>
+				{/await}
+			</div>
+			<div class="hover:cursor-pointer w-[40px] h-[36px] bg-[#283848] text-center rounded-full self-center flex items-center justify-center">
+				<span>></span>
+			</div>
+		</div>
+	</div>
+	<div class="2xl:order-last">
+		<p class="text-5xl mb-2">Люди</p>
+		<div class="flex justify-start">
+			<div class="h-[22rem] flex flex-wrap overflow-hidden gap-4 flex-grow-0">
+				{#await popularMovies}
+				<SkeletonPanel/>
+				{:then result}
+					{#each result as film}
+						<MoviePanel movie={film} />
+					{/each}
+				{:catch error}
+					<div class="flex rounded-lg bg-gray-700 h-80 w-48 text-white">
+						<p class="text-lg">{error.stack}</p>
+					</div>
+				{/await}
+			</div>
+			<div class="hover:cursor-pointer w-[40px] h-[36px] bg-[#283848] text-center rounded-full self-center flex items-center justify-center">
+				<span>></span>
+			</div>
+		</div>
+	</div>
+	<div>
+		НОВОСТИ
 	</div>
 </div>
